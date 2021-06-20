7a58ce0b39f01f4abc26a2a77777e92c239e0105
Gabor Csecsetka <csecsi85@gmail.com>
2021-06-21 00:14:38 +0100

    Starter add application form component

--- components/layout/main-header.js
+++ components/layout/main-header.js
@@ -1,15 +1,19 @@
 import Link from 'next/link';
+import classes from './main-header.module.css';
 
 function MainHeader() {
   return (
-    <header>
+    <header className={classes['main-header']}>
       <div>
         <Link href="/">Logo</Link>
       </div>
       <nav>
-        <ul>
+        <ul className={classes['nav-list']}>
           <li>
             <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/add-application">New Application</Link>
           </li>
         </ul>
       </nav>
